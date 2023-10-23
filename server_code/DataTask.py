563b8689e940096002e1677a3f06526fefa5c305
darigan.alex@gmail.com <darigan.alex@gmail.com>
2023-10-23 14:42:33 +0000

    Edited DataTask

--- server_code/DataTask.py
+++ server_code/DataTask.py
@@ -46,7 +46,7 @@ def getData():
   print(downloaded.is_complete())
   print(downloaded.content_type)
 
-  json_str = download.get_bytes().decode('utf-8')
+  json_str = downloaded.get_bytes().decode('utf-8')
   # Parse the JSON string into a Python dictionary
   data = json.loads(json_str)
   
