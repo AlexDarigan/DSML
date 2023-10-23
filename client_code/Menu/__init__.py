fe857a7ac2f7cdb700fd49079ef78193cea9c820
darigan.alex@gmail.com <darigan.alex@gmail.com>
2023-10-23 12:26:43 +0000

    Create Skeleton

---
+++ client_code/Menu/__init__.py
@@ -0,0 +1,24 @@
+from ._anvil_designer import MenuTemplate
+from anvil import *
+
+class Menu(MenuTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+
+  def home_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('Home')
+
+  def about_me_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('Bio')
+
+  def projects_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('DSML')
+
+
+
