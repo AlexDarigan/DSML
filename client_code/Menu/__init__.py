a8cc18d43e7946623515855f8435fe20dbd47be9
darigan.alex@gmail.com <darigan.alex@gmail.com>
2023-10-26 14:18:18 +0000

    Edited Menu; Deleted Home

--- client_code/Menu/__init__.py
+++ client_code/Menu/__init__.py
@@ -9,14 +9,7 @@ import anvil.server
 
 class Menu(MenuTemplate):
   def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
-    # Any code you write here will run before the form opens.
-
-  def home_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    open_form('Home')
 
   def about_me_click(self, **event_args):
     """This method is called when the button is clicked"""
